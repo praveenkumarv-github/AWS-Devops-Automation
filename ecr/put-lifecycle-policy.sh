6b6a3642fbd8baee7f6957eadaccb45332792db3
Praveen-Tri <praveenkumar@tricog.com>
2022-09-05 10:22:34 +0530

    removed duplicatted commands

--- ecr/put-lifecycle-policy.sh
+++ ecr/put-lifecycle-policy.sh
@@ -1,30 +1,7 @@
 aws ecr put-lifecycle-policy \
     --repository-name "sample-app-ui" \
     --lifecycle-policy-text "file://policy.json"
-aws ecr put-lifecycle-policy \
-    --repository-name "sample_panel_backend" \
-    --lifecycle-policy-text "file://policy.json"
-aws ecr put-lifecycle-policy \
-    --repository-name "sample-app-api" \
-    --lifecycle-policy-text "file://policy.json"
-aws ecr put-lifecycle-policy \
-    --repository-name "sample" \
-    --lifecycle-policy-text "file://policy.json"
-aws ecr put-lifecycle-policy \
-    --repository-name "sample_server" \
-    --lifecycle-policy-text "file://policy.json"
-aws ecr put-lifecycle-policy \
-    --repository-name "sample-container-assets-609249146283-ap-southeast-1" \
-    --lifecycle-policy-text "file://policy.json"
-aws ecr put-lifecycle-policy \
-    --repository-name "sample-docs" \
-    --lifecycle-policy-text "file://policy.json"
-aws ecr put-lifecycle-policy \
-    --repository-name "sample" \
-    --lifecycle-policy-text "file://policy.json"
-aws ecr put-lifecycle-policy \
-    --repository-name "sample_panel_frontend" \
-    --lifecycle-policy-text "file://policy.json"
+
 
 
 
